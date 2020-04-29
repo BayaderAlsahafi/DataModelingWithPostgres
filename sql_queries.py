40c96b39acf9377b3261529dce307bc6e5879a5e
Bayader Alsahafi <bayader97@gmail.com>
2020-04-29 14:08:28 +0300

    Completed Create and Drop statements

--- sql_queries.py
+++ sql_queries.py
@@ -1,45 +1,66 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays (songplay_id int PRIMARY KEY,\
+                                                user_id int REFERENCES users (user_id),\
+                                                song_id varchar REFERENCES songs (song_id),\
+                                                artist_id varchar REFERENCES artists (artist_id),\
+                                                start_time float REFERENCES time (start_time),\
+                                                level varchar,\
+                                                session_id int,\
+                                                location varchar,\
+                                                user_agent varchar);")
 
-user_table_create = ("""
-""")
+user_table_create = ("CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY,\
+                                                    first_name varchar,\
+                                                    last_name varchar,\
+                                                    gender varchar,\
+                                                    level varchar);")
 
-song_table_create = ("""
-""")
+song_table_create = ("CREATE TABLE IF NOT EXISTS songs (song_id varchar PRIMARY KEY,\
+                                                    title varchar,\
+                                                    artist_id varchar REFERENCES artists (artist_id),\
+                                                    year int,\
+                                                    duration float);")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id varchar PRIMARY KEY,\
+                                                        name varchar,\
+                                                        location varchar,\
+                                                        latitude varchar,\
+                                                        longitude varchar);")
 
-time_table_create = ("""
-""")
+time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time float PRIMARY KEY,\
+                                                    hour int,\
+                                                    day int,\
+                                                    week int,\
+                                                    month int,\
+                                                    year int,\
+                                                    weekday varchar);")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = ("")
+#INSERT INTO songplay (songplay_id, user_id, song_id, artist_id,start_time, level, session_id, location, user_agent) VALUES ();
 
-user_table_insert = ("""
-""")
+user_table_insert = (""
+"")
 
-song_table_insert = ("""
-""")
+song_table_insert = (""
+"")
 
-artist_table_insert = ("""
-""")
+artist_table_insert = (""
+"")
 
 
-time_table_insert = ("""
-""")
+time_table_insert = (""
+"")
 
 # FIND SONGS
 
@@ -47,6 +68,5 @@ song_select = ("""
 """)
 
 # QUERY LISTS
-
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, time_table_create, song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
